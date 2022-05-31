f801f0a4776c061450eb21dcf198f1973d3f3172
lucciyun <3512545450@qq.com>
2022-05-31 17:00:17 +0800

    添加cb

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import MainPage from "./pages/MainPage";
 import { ThemeProvider } from "styled-components";
 import { DarkTheme } from "./Themes";
@@ -6,22 +6,20 @@ import GlobalStyle from "./GlobalStyle";
 import { useEffect } from "react";
 import md5 from "js-md5";
 
-const auth = sessionStorage.getItem('isAuth')
+const auth = sessionStorage.getItem("isAuth");
 
 const App = () => {
   const [isAuth, setIsAuth] = useState<number>(
-    (auth && auth !== "undefined" && auth !== null) ? JSON.parse(auth) : 0
-  )
-  const handlePwdSecure = () => {
+    auth && auth !== "undefined" && auth !== null ? JSON.parse(auth) : 0
+  );
+  const handlePwdSecure = useCallback(() => {
     const pwdSecure = localStorage.getItem("pwdSecure");
     if (!pwdSecure) {
-      const pwd = window.prompt(
-        "请保存您的密码，该密码在下次登录时使用!"
-      );
+      const pwd = window.prompt("请保存您的密码，该密码在下次登录时使用!");
       if (pwd) {
         localStorage.setItem("pwdSecure", md5(pwd!));
-        setIsAuth(1)
-        sessionStorage.setItem('isAuth', '1')
+        setIsAuth(1);
+        sessionStorage.setItem("isAuth", "1");
       } else {
         handlePwdSecure();
       }
@@ -31,20 +29,20 @@ const App = () => {
         if (md5(pwd!) !== pwdSecure) {
           handlePwdSecure();
         } else {
-          setIsAuth(1)
-          sessionStorage.setItem('isAuth', '1')
+          setIsAuth(1);
+          sessionStorage.setItem("isAuth", "1");
         }
       } else {
         handlePwdSecure();
       }
     }
-  };
+  }, []);
 
   useEffect(() => {
     if (isAuth !== 1) {
-      handlePwdSecure()
+      handlePwdSecure();
     }
-  }, []);
+  }, [handlePwdSecure, isAuth]);
 
   return (
     <>
